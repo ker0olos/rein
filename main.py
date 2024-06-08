7439ddc58d5daf8e51046457ea1fb2d6f7af2b25
Kerolos Zaki <ker0olos@pm.me>
2024-06-08 14:55:48 +0300

    feat: added fastapi to stream images to a localhost url

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import cairosvg
 import cv2
 import numpy as np
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
 from src.svg.model import Model
 from src.tracking.tracking import Tracking
 
-with open("models/face.svg", "r") as file:
-    img = cv2.imread("tests/test_eyes_mouth.jpeg")
-    numpy_array = np.array(img)
-    #
-    xml_string = file.read()
-    model = Model(xml_string)
-    #
-    tracking = Tracking()
-    tracking.set_model(model)
-    tracking.process(numpy_array)
-    # print(model.tostring())
-    cairosvg.svg2png(bytestring=model.tostring().encode(), write_to="output.png")
+app = FastAPI()
+
+
+async def fake_video_streamer():
+    with open("models/face.svg", "r") as file:
+        img = cv2.imread("tests/test_eyes_mouth.jpeg")
+        numpy_array = np.array(img)
+        #
+        xml_string = file.read()
+        model = Model(xml_string)
+        #
+        tracking = Tracking()
+        tracking.set_model(model)
+        tracking.process(numpy_array)
+        # print(model.tostring())
+        #
+        output_image = cairosvg.svg2png(bytestring=model.tostring().encode())
+        #
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/png\r\n\r\n" + bytearray(output_image) + b"\r\n"
+        )
+
+
+@app.get("/")
+async def video_stream():
+    return StreamingResponse(
+        fake_video_streamer(), media_type="multipart/x-mixed-replace;boundary=frame"
+    )
