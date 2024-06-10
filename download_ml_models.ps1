28c20dca5a3f6addf0138afc9ad5461956e6557f
Kerolos Zaki <ker0olos@pm.me>
2024-06-10 20:24:43 +0300

    fix tests

---
+++ download_ml_models.ps1
@@ -0,0 +1,2 @@
+#!/usr/bin/env pwsh
+Invoke-WebRequest -Uri "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task" -OutFile "face_landmarker.task"
